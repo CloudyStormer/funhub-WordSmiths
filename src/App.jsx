55b07009bb7163807da6d2125c1e1a17732f2941
bruce <295572084@qq.com>
2026-04-02 22:52:28 +0800

    1

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
+    <BrowserRouter basename="/funhub/wordSmiths">
       <Routes>
         <Route path="/" element={<Dashboard />} />
       </Routes>
